7ef7b4b8f7a860437d3cda03f7cbb928ae3f005e
Jeff Becker <ampernand@gmail.com>
2016-05-21 08:11:59 -0400

    * update readme to reflect reality
    * add docs to config

--- botemail.lua
+++ botemail.lua
@@ -6,14 +6,29 @@ bind = ":2525"
 domain = "i2p.rocks"
 maildir = "/tmp/mail"
 
+
+--
+-- the following functions are required to be defined
+--
+
+
+--
+-- return 1 if this mail is whitelisted otherwise return 0
+--
 function whitelist(addr, recip, sender, body)
    return 0
 end
 
+--
+-- return 1 if this mail is blacklisted otherwise return 0
+--
 function blacklist(addr, recip, sender, body)
    return 0
 end
 
+--
+-- return 1 if this mail is spam otherwise return 0
+--
 function checkspam(addr, recip, sender, body)
    return 0
 end
--- readme.md
+++ readme.md
@@ -4,8 +4,7 @@ server that runs i2p.rocks email
 
 ## features ##
 
-* smtp over tls by default
-* portable install using sqlite database
-* extendable via lua
-* brain dead simple configuration
+* brain dead simple configuration (see botemail.lua)
+* brain dead simple database backend (sqlite3)
 * brain dead simple license (MIT)
+* extendable via lua
