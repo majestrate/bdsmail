cfb659d679dd6b6c021b5b661a7e52282a9fd26b
Jeff Becker <ampernand@gmail.com>
2016-05-21 09:15:58 -0400

    fix bdsconfig

--- cmd/bdsconfig/main.go
+++ cmd/bdsconfig/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 )
 
 // bds mail configuration generator
@@ -11,19 +12,22 @@ func main() {
 	domain := "localhost"
 	bind := ":2525"
 	maildir := "mail"
-
+	
 	if len(os.Args) > 1 {
 		domain = os.Args[1]
 	}
 	if len(os.Args) > 2 {
 		maildir = os.Args[2]
 	}
-
-	fmt.Fprintf("-- auto generated config made %s\n", time.Now().Format(time.ANSIC))
-	fmt.Fprintf(`bind = ":%s"\n`, bind)
-	fmt.Fprintf(`domain = "%s"\n`, domain)
-	fmt.Fprintf(`maildir = "%s"\n`, maildir)
-	for _, funcname := range []string{"whitelist", "blacklist", "checkspam"} {
-		fmt.Fprintf(`\n\nfunction %s(addr, recip, sender, body)\nreturn 0\nend\n\n`, funcname)
+	
+	fmt.Printf("-- auto generated config made %s\n", time.Now().Format(time.ANSIC))
+	fmt.Printf(`bind = "%s"`, bind)
+	fmt.Printf("\n")
+	fmt.Printf(`domain = "%s"`, domain)
+	fmt.Printf("\n")
+	fmt.Printf(`maildir = "%s"`, maildir)
+	fmt.Printf("\n")
+	for _, funcname := range []string {"whitelist", "blacklist", "checkspam"} {
+		fmt.Printf("\n\nfunction %s(addr, recip, sender, body)\nreturn 0\nend\n\n", funcname)
 	}
 }
