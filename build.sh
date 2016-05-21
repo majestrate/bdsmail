ebb6589859e6e25ee02ccd565095abe0e9937dd3
Jeff Becker <ampernand@gmail.com>
2016-05-21 09:13:16 -0400

    fix build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,7 @@
 set -e
 export GOPATH=$PWD/go
 
-go get -u -v github.com/majestrate/bdsmail/
-go install -v github.com/majestrate/bdsmail/cmd/bdsconfig
-go install -v github.com/majestrate/bdsmail/cmd/bdsmail
+go get -v github.com/majestrate/bdsmail/cmd/bdsconfig
+go get -v github.com/majestrate/bdsmail/cmd/bdsmail
 cp $GOPATH/go/bin/bdsmail $PWD
 cp $GOPATH/go/bin/bdsconfig $PWD
