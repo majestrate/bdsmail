4527bfa091b3093ccd69abfd0a4f8e69b7a7f7e3
zaphodbblrox <justthisguyyouknow.github@nigge.rs>
2016-06-02 05:55:47 -0700

    Remove environement variables
    There's no need for them unless we're going to be jumping around.

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #/usr/bin/env bash
 set -e
-export GOPATH=go
 
 go get -u -v github.com/majestrate/bdsmail/cmd/bdsconfig
 go get -u -v github.com/majestrate/bdsmail/cmd/bdsmail
-cp "$GOPATH"/bin/bdsmail .
-cp "$GOPATH"/bin/bdsconfig .
+cp go/bin/bdsmail .
+cp go/bin/bdsconfig .
