35fce8d9b717a2ab74e22f85e420559af7cde25c
Jeff Becker <ampernand@gmail.com>
2016-08-05 17:20:23 -0400

    syntax error

--- src/bds/lib/server/util.go
+++ src/bds/lib/server/util.go
@@ -10,7 +10,7 @@ func parseFromI2PAddr(email string) (name string) {
   idx_at := strings.Index(email, "@")
   if strings.HasSuffix(email, ".b32.i2p") {
     name = email[idx_at+1:]
-  } else if strings.HasSuffix(email, ".i2p")
+  } else if strings.HasSuffix(email, ".i2p") {
     idx_i2p := strings.LastIndex(email, ".i2p")    
     name = fmt.Sprintf("smtp.%s.i2p", email[idx_at+1:idx_i2p])
   }
